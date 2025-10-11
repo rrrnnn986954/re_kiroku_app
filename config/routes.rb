446fdebcdb1e33383b5b767b22b0d795176835fc
rrrnnn986954 <negima6954@gmail.com>
2025-10-11 20:58:55 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "records#index"
-
-  resources :sessions, only: [:create, :update]
   resources :records, only: [:index, :show]
 
+  resources :sessions, only: [:create, :update]
   resources :actions, only: [:create, :update, :edit]
+  
+
 end
